10c5929d7daa3ebd2a8673483e634c751216e7b2
edmilson-dk <droidvidaboa@gmail.com>
2021-05-28 11:01:31 -0300

    style: added loading class global styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,4 +1,12 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, keyframes } from "styled-components";
+
+const loadingAnimate = keyframes`
+  0% {
+    transform: rotate(0);
+  }100% {
+    transform: rotate(360deg);
+  }
+`;
 
 export const GlobalsStyles = createGlobalStyle`
   *,
@@ -7,13 +15,35 @@ export const GlobalsStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    background-color: #;
   }
 
   .flex-center {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  .loading {
+    width: 100%;
+    min-height: 100vh;
+    background-color: rgba(244, 244, 244, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    
+    > div {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      border-top: 5px solid #3cc83b;
+      border-bottom: 5px solid #3cc83b;
+      border-left: 5px solid #ccc;
+      border-right: 5px solid #ccc;
+
+      animation: ${loadingAnimate} .8s ease-in-out infinite;
+    }
   }
 
   html {
