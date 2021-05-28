c7ba263b9ceee7d7da75c80fdb5373aa85825681
edmilson-dk <droidvidaboa@gmail.com>
2021-05-28 15:35:32 -0300

    feat: added use movie context custom hook

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,76 +1,25 @@
-import { FormEvent, useRef, useState } from "react";
 import { FiSearch, FiStar } from "react-icons/fi";
 
 import { ButtonRedirect } from "src/components/button-redirect";
 import { Loading } from "src/components/loading";
 import { MovieInfosModal } from "src/components/movie-infos-modal";
 import { MoviePreviewCard } from "src/components/movie-preview-card";
-import { axiosFetchApi } from "src/services/fetch-api";
+import { useMovieContext } from "src/contexts/movie-context";
 import { Container } from "src/styles/components/container";
 import { HomeContainer, HomeContent, HomeContentContainer, HomeWrapper } from "src/styles/screens/home";
-import { MovieInfosDataApiResponse, MoviePreviewDataApiResponse } from "src/types/api-response-types";
-import { toTopAndAddNotScroll } from "src/utils/toTopAndAddNotScroll";
 
 export function Home() {
-  const [moviesDataPreview, setMoviesDataPreview] = useState([] as MoviePreviewDataApiResponse[]);
-  const [moviesDataInfos, setMoviesDataInfos] = useState({} as MovieInfosDataApiResponse);
-  const searchInputRef = useRef<HTMLInputElement>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isNotFoundMovie, setIsNotFoundMovie] = useState(false);
-  const [isOpenModalInfos, setIsOpenModalInfos] = useState(false);
-
-  function handleCloseModal() {
-    toTopAndAddNotScroll(false, false);
-    setIsOpenModalInfos(false);
-  }
-
-  async function handleSearchOneMovieInfos(id: string) {
-    setIsLoading(true);
-    toTopAndAddNotScroll(true, true);
-
-    const response = await axiosFetchApi.get<MovieInfosDataApiResponse>(`/search/infos/${id}`);
-
-    if (response.data) {
-      setIsLoading(false);
-      setIsOpenModalInfos(true);
-    }
-
-    setMoviesDataInfos(response.data);
-  }
-
-  async function handleSearchAllMovies(e: FormEvent) {
-    e.preventDefault();
-
-    try {
-      if (searchInputRef.current && searchInputRef.current.value.length >= 2) {
-        setIsLoading(true);
-        toTopAndAddNotScroll(true);
-
-        const response = await axiosFetchApi.get<MoviePreviewDataApiResponse[] | []>(
-          "/search/all",
-          {
-            params: { title: searchInputRef.current.value.trimEnd().trimStart() },
-          }
-        );
-
-        if (response.data.length > 0) {
-          setIsLoading(false);
-          toTopAndAddNotScroll(false);
-          setIsNotFoundMovie(false);
-        } else if (response.data.length === 0) {
-          setIsLoading(false);
-          setIsNotFoundMovie(true);
-        }
-
-        setMoviesDataPreview(response.data);
-      } else {
-        alert("The search must have at least two characters");
-      }
-    } catch(e) {
-      setIsLoading(false);
-      setIsNotFoundMovie(true);
-    }
-  }
+  const {
+    handleCloseModal,
+    handleSearchOneMovieInfos,
+    handleSearchAllMovies,
+    moviesDataPreview,
+    moviesDataInfos,
+    isLoading,
+    isNotFoundMovie,
+    isOpenModalInfos,
+    searchInputRef,
+  } = useMovieContext();
 
   return (
     <>
