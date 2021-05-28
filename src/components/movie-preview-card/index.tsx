3f0891f82c20e580516139b4e0a39c21ba01df26
edmilson-dk <droidvidaboa@gmail.com>
2021-05-28 15:12:09 -0300

    feat: added onclick open modal handle function

--- src/components/movie-preview-card/index.tsx
+++ src/components/movie-preview-card/index.tsx
@@ -1,13 +1,13 @@
 import { FiStar } from "react-icons/fi";
 
-import { MoviePreviewCardProps } from "./types";
 import BannerNotFoundPng from "src/assets/banner-notfound.png";
+import { MoviePreviewCardProps } from "./types";
 import { MoviePreviewCardContent, MoviePreviewCardWrapper } from "src/styles/components/movie-preview-card";
 
-export function MoviePreviewCard({ data }: MoviePreviewCardProps) {
+export function MoviePreviewCard({ data, displayInfosClick }: MoviePreviewCardProps) {
   return (
     <MoviePreviewCardWrapper>
-      <header>
+      <header onClick={() => displayInfosClick(data.id)}>
         <img 
           src={data.poster === "N/A" ? BannerNotFoundPng : data.poster} 
           alt={data.poster === "N/A" ? "https://br.freepik.com/vetores/abstrato" : "Movie banner"}
@@ -15,7 +15,7 @@ export function MoviePreviewCard({ data }: MoviePreviewCardProps) {
       </header>
 
       <MoviePreviewCardContent>
-        <article>
+        <article onClick={() => displayInfosClick(data.id)}>
           <h3>{data.title}</h3>
           <span>Type: {data.type[0].toUpperCase() + data.type.substr(1)}</span>
           <time>Year: {data.year}</time>
