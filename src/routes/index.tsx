e274828ac16572e37c2050d6fedf2adffcfa7a55
edmilson-dk <droidvidaboa@gmail.com>
2021-05-29 10:19:04 -0300

    feat: added favorits router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { Favorites } from "src/screens/favorites";
 import { Home } from "src/screens/home";
 
 export function Routes() {
@@ -7,6 +8,7 @@ export function Routes() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/favorites" component={Favorites}/>
       </Switch>
     </BrowserRouter>
   )
