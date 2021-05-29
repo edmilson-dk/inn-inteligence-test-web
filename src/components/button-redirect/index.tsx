e0b281ce0684c830cfc64469d4d16026fa664a00
edmilson-dk <droidvidaboa@gmail.com>
2021-05-28 22:11:30 -0300

    feat: added link of react router dom

--- src/components/button-redirect/index.tsx
+++ src/components/button-redirect/index.tsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 import { ButtonRedirectWrapper } from "../../styles/components/button-redirect";
 import { ButtonRedirectProps } from "./types";
 
 export function ButtonRedirect({ label, redirectPath, icon }: ButtonRedirectProps) {
   return (
-    <ButtonRedirectWrapper className="flex-center">
-      { label }
-      <span>
-        { icon }
-      </span>
-    </ButtonRedirectWrapper>
+    <Link to={redirectPath}> 
+      <ButtonRedirectWrapper className="flex-center">
+        { label }
+        <span>
+          { icon }
+        </span>
+      </ButtonRedirectWrapper>
+    </Link>
   )
 }
