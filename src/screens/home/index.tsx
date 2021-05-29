af3afb1550f17ab73b31367cc0b30e69420da008
edmilson-dk <droidvidaboa@gmail.com>
2021-05-29 10:19:29 -0300

    feat: added addMovieFavorite button click action

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { FiSearch, FiStar } from "react-icons/fi";
+import { ActionInfoModal } from "src/components/action-info-modal";
 
 import { ButtonRedirect } from "src/components/button-redirect";
 import { Loading } from "src/components/loading";
@@ -8,10 +9,13 @@ import { MovieFilterData } from "src/components/movie-filter-data";
 import { MovieInfosModal } from "src/components/movie-infos-modal";
 import { MoviePreviewCard } from "src/components/movie-preview-card";
 import { useMovieContext } from "src/contexts/movie-context";
+import { useMoviesFavoritesContext } from "src/contexts/movies-favorites-context";
 import { Container } from "src/styles/components/container";
-import { HomeContainer, HomeContent, HomeContentContainer, HomeWrapper } from "src/styles/screens/home";
+import { ScreenContainer, ScreenContent, ScreenContentContainer, ScreenWrapper } from "src/styles/screens/generic";
+import { toTopAndAddNotScroll } from "src/utils/toTopAndAddNotScroll";
 
 export function Home() {
+  const { addMovieInFavorites } = useMoviesFavoritesContext()
   const {
     handleCloseModal,
     handleSearchOneMovieInfos,
@@ -23,11 +27,20 @@ export function Home() {
     isOpenModalInfos,
     searchInputRef,
     moviesPreviewTotal,
-    searchAllMoviesWithQuery
+    searchAllMoviesWithQuery,
+    setMoviesDataPreview
   } = useMovieContext();
+
   const [actualPage, setActualPage] = useState(1);
   const [type, setType] = useState("movie");
   const [year, setYear] = useState(0);
+  const [isOpenModalAction, setIsOpenModalAction] = useState(false);
+
+  async function handleAddFavorite(id: string) {
+    await addMovieInFavorites(id);
+    toTopAndAddNotScroll(false, true);
+    setIsOpenModalAction(true);
+  }
 
   async function handlerNextDataClick() {
     const title = searchInputRef.current ? searchInputRef.current.value : "";
@@ -43,6 +56,8 @@ export function Home() {
     setActualPage(page);
   }
 
+  useEffect(() => setMoviesDataPreview([]), []);
+
   useEffect(() => {
     (async () => {
       if (searchInputRef.current && searchInputRef.current.value) {
@@ -55,14 +70,14 @@ export function Home() {
 
   return (
     <>
-      <HomeWrapper>
+      <ScreenWrapper>
         <ButtonRedirect
           label="Favorites"
           redirectPath="/favorites"
           icon={<FiStar size="100%" />}
         />
         <Container>
-          <HomeContainer>
+          <ScreenContainer>
             <h1 className="flex-center">
               Movix
               <span>
@@ -84,9 +99,16 @@ export function Home() {
                   setMovieYear={setYear}
                 />)
             }
-          </HomeContainer>
+          </ScreenContainer>
         </Container>
-      </HomeWrapper>
+      </ScreenWrapper>
+      
+      {isOpenModalAction && (
+        <ActionInfoModal 
+          message="Successfully added item" 
+          closeModalState={setIsOpenModalAction}
+        />)
+      }
 
       {isLoading && (<Loading />)}
 
@@ -99,20 +121,26 @@ export function Home() {
         />
       )}
 
-      <HomeContent style={{
+      <ScreenContent style={{
         display: moviesDataPreview.length > 0 ? "flex" : "none",
         top: (isOpenModalInfos || isLoading) ? "100vh" : "calc(100vh - 100px)",
       }}>
         <Container>
-          <HomeContentContainer>
+          <ScreenContentContainer>
             {
               moviesDataPreview.length > 0 && moviesDataPreview.map(item => (
-                <MoviePreviewCard displayInfosClick={handleSearchOneMovieInfos} key={item.id} data={item} />
+                <MoviePreviewCard 
+                  displayInfosClick={handleSearchOneMovieInfos} 
+                  key={item.id} 
+                  data={item}
+                  buttonActionClick={() => handleAddFavorite(item.id)}
+                  buttonIcon={<FiStar />}
+                />
               ))
             }
-          </HomeContentContainer>
+          </ScreenContentContainer>
           {
-            moviesDataPreview.length > 0 && (
+            moviesDataPreview.length >= 10 && (
               <MovieDataPaginate
                 nextCbFetch={handlerNextDataClick}
                 prevCbFetch={handlerPrevDataClick}
@@ -122,7 +150,7 @@ export function Home() {
             )
           }
         </Container>
-      </HomeContent>
+      </ScreenContent>
     </>
   )
 }
