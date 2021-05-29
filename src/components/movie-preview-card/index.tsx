651de9f22617819cab04f067595bad0d9fea7f19
edmilson-dk <droidvidaboa@gmail.com>
2021-05-29 10:17:32 -0300

    feat: added new property

--- src/components/movie-preview-card/index.tsx
+++ src/components/movie-preview-card/index.tsx
@@ -4,7 +4,7 @@ import BannerNotFoundPng from "src/assets/banner-notfound.png";
 import { MoviePreviewCardProps } from "./types";
 import { MoviePreviewCardContent, MoviePreviewCardWrapper } from "src/styles/components/movie-preview-card";
 
-export function MoviePreviewCard({ data, displayInfosClick }: MoviePreviewCardProps) {
+export function MoviePreviewCard({ data, displayInfosClick, buttonActionClick, buttonIcon }: MoviePreviewCardProps) {
   return (
     <MoviePreviewCardWrapper>
       <header onClick={() => displayInfosClick(data.id)}>
@@ -18,13 +18,13 @@ export function MoviePreviewCard({ data, displayInfosClick }: MoviePreviewCardPr
         <article onClick={() => displayInfosClick(data.id)}>
           <h3>{data.title}</h3>
           <span>Type: {data.type[0].toUpperCase() + data.type.substr(1)}</span>
-          <time>Year: {data.year}</time>
+          <time>Year: {data.year ?? data.released}</time>
         </article>
 
         <footer>
-          <button type="button">
+          <button type="button" onClick={buttonActionClick}>
             <span>
-              <FiStar size="100%" />
+             { buttonIcon }
             </span>
           </button>
         </footer>
